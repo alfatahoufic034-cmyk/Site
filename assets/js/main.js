dc7dbb9396660b84901af2f62b6c51cc599d9c35
alfatahoufic034-cmyk <alfatahoufic034@gmail.com>
2026-06-07 19:11:23 +0000

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,25 @@ document.getElementById("registerForm").addEventListener("submit", function(e){
 
   this.reset();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const menuToggle = document.getElementById("menu-toggle");
+  const navMenu = document.getElementById("nav-menu");
+
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+
+});
